623f848a23d04955d10047e2edd62f3d5e25b8df
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-01-30 14:39:04 +0100

    correction

--- table.sql
+++ table.sql
@@ -24,37 +24,24 @@ CREATE TABLE `Orders`(
     `status` varchar(255) NOT NULL,
     `updated_at` varchar(255) NOT NULL DEFAULT current_timestamp(),
     `created_at` int(11) NOT NULL DEFAULT current_timestamp()
+    FOREIGN KEY (`id_user`) REFERENCES Users (`id`)
 );
-ALTER TABLE Orders
-    ADD CONSTRAINT `name`
-    FOREIGN KEY (`id_user`)
-    REFERENCES Users (`id`)
-    ON DELETE CASCADE;
 
 CREATE TABLE `Avis`(
     `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `id_user` INT(11) NOT NULL,
-    `id` INT(11) NOT NULL,
-    `commentaire` varchar(255) NOT NULL,
+    `id_product` INT(11) NOT NULL,
+    `comment` varchar(255) NOT NULL,
     `note` FLOAT(1) NOT NULL,
     `uploaded_at` int(11) NOT NULL DEFAULT current_timestamp(),
-    FOREIGN KEY (`id_user`)REFERENCES Users (`id`),
-    FOREIGN KEY (`id`)REFERENCES Orders (`id`),
+    FOREIGN KEY (`id_user`) REFERENCES Users (`id`),
+    FOREIGN KEY (`id_product`) REFERENCES Products (`id`)
 );
 
 CREATE TABLE `Link`(
     `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `id_order` INT(11) NOT NULL,
     `id_product` INT(11) NOT NULL,
+    FOREIGN KEY (`id_order`) REFERENCES Orders (`id`),
+    FOREIGN KEY (`id_product`) REFERENCES Products (`id`)
 );
-ALTER TABLE Link
-    ADD CONSTRAINT `name`
-    FOREIGN KEY (`id_order`)
-    REFERENCES Orders (`id`)
-    ON DELETE CASCADE;
-
-ALTER TABLE Link
-    ADD CONSTRAINT `name`
-    FOREIGN KEY (`id_product`)
-    REFERENCES Products (`id`)
-    ON DELETE CASCADE;
