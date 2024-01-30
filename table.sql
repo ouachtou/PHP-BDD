0322f6fb078710312fe3a9a6de34e100b75f02d9
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-01-30 14:29:10 +0100

    order table

--- table.sql
+++ table.sql
@@ -6,17 +6,27 @@ CREATE TABLE `Users` (
     `password` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-    `admin` boolean DEFAULT
+    `admin` boolean NOT NULL
 );
 
-CREATE TABLE 'Products' (
-    `id_product` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Products` (
+    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `type` varchar(255) NOT NULL,
     `price` int(11) NOT NULL,
     `quantity` int(11) NOT NULL,
-    `reduction` int(11) NOT NULL,
+    `reduction` int(11) NOT NULL
 );
 
-CREATE TABLE 'Orders'(
-)
+CREATE TABLE `Orders`(
+    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `id_user` INT(11) NOT NULL,
+    `status` varchar(255) NOT NULL,
+    `updated_at` varchar(255) NOT NULL DEFAULT current_timestamp(),
+    `created_at` int(11) NOT NULL DEFAULT current_timestamp()
+);
+ALTER TABLE Orders
+    ADD CONSTRAINT `name`
+    FOREIGN KEY (`id_user`)
+    REFERENCES Users (`id`)
+    ON DELETE CASCADE;
