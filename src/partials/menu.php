4f2399cc44749405a2c6fc55a7dfa7591ff46a68
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-01-30 15:56:03 +0100

    admin needed to add product

--- src/partials/menu.php
+++ src/partials/menu.php
@@ -1,21 +1,26 @@
+<?php
+require_once __DIR__ . '/../init.php';
+?>
+
 <ul>
     <li>
         <a href="/">Home</a>
     </li>
     <?php
     if ($user === false) { ?>
-    <li>
-        <a href="/register.php">Register</a>
-    </li>
-    <li>
-        <a href="/login.php">Login</a>
-    </li>
-    <li>
-        <a href="/addProduct.php">Add Product</a>
-    </li>
+        <li>
+            <a href="/register.php">Register</a>
+        </li>
+        <li>
+            <a href="/login.php">Login</a>
+        </li>
+    <?php } elseif ($user->admin) { ?>
+        <li>
+            <a href="/addProduct.php">Add Product</a>
+        </li>
     <?php } else { ?>
-    <li>
-        <a href="/actions/logout.php">Log OUT</a>
-    </li>
+        <li>
+            <a href="/actions/logout.php">Log OUT</a>
+        </li>
     <?php } ?>
 </ul>
