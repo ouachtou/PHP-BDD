10fe3d77f0ce04a8b92e5be2510900568a26f069
ouachtou <pierre.legrand1105@gmail.com>
2024-01-30 21:16:01 +0100

    sql

--- table.sql
+++ table.sql
@@ -1,47 +1,69 @@
 CREATE TABLE `Users` (
-    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `first_name` varchar(255) NOT NULL,
-    `adress` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-    `admin`boolean DEFAULT FALSE
-);
+    `id`            INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`          varchar(255) NOT NULL,
+    `first_name`    varchar(255) NOT NULL,
+    `adress`        varchar(255) NOT NULL,
+    `password`      varchar(255) NOT NULL,
+    `email`         varchar(255) NOT NULL,
+    `created_at`    datetime NOT NULL DEFAULT current_timestamp(),
+    `admin`         boolean DEFAULT FALSE
+)
+ENGINE = INNODB
+CHARACTER SET 'utf8';
+
+
 
 CREATE TABLE `Products` (
-    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `type` varchar(255) NOT NULL,
-    `price` int(11) NOT NULL,
-    `quantity` int(11) NOT NULL,
-    `reduction` int(11) NOT NULL
-);
+    `id`            INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`          varchar(255) NOT NULL,
+    `type`          varchar(255) NOT NULL,
+    `price`         int(11) NOT NULL,
+    `quantity`      int(11) NOT NULL,
+    `reduction`     int(11) NOT NULL
+)
+ENGINE = INNODB
+CHARACTER SET 'utf8';
+
+
+
 
 CREATE TABLE `Orders`(
-    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `id_user` INT(11) NOT NULL,
-    `status` varchar(255) NOT NULL,
-    `updated_at` varchar(255) NOT NULL DEFAULT current_timestamp(),
-    `created_at` int(11) NOT NULL DEFAULT current_timestamp()
-    FOREIGN KEY (`id_user`) REFERENCES Users (`id`)
-);
+    `id`            int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `id_user`       int(11) NOT NULL,
+    `status`        varchar(255) NOT NULL,
+    `updated_at`    datetime NOT NULL DEFAULT current_timestamp(),
+    `created_at`    datetime NOT NULL DEFAULT current_timestamp(),
+
+    FOREIGN KEY (`id_user`) REFERENCES Users (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE = INNODB
+CHARACTER SET 'utf8';
+
+
 
 CREATE TABLE `Avis`(
-    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `id_user` INT(11) NOT NULL,
-    `id_product` INT(11) NOT NULL,
-    `comment` varchar(255) NOT NULL,
-    `note` FLOAT(1) NOT NULL,
-    `uploaded_at` int(11) NOT NULL DEFAULT current_timestamp(),
-    FOREIGN KEY (`id_user`) REFERENCES Users (`id`),
-    FOREIGN KEY (`id_product`) REFERENCES Products (`id`)
-);
+    `id`            int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `id_user`       int(11) NOT NULL,
+    `id_product`    int(11) NOT NULL,
+    `comment`       varchar(255) NOT NULL,
+    `note`          float(1) NOT NULL,
+    `uploaded_at`   datetime NOT NULL DEFAULT current_timestamp(),
+
+    FOREIGN KEY (`id_user`) REFERENCES Users (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`id_product`) REFERENCES Products (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE = INNODB
+CHARACTER SET 'utf8';
+
+
 
 CREATE TABLE `Link`(
-    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `id_order` INT(11) NOT NULL,
-    `id_product` INT(11) NOT NULL,
-    FOREIGN KEY (`id_order`) REFERENCES Orders (`id`),
-    FOREIGN KEY (`id_product`) REFERENCES Products (`id`)
-);
+    `id`            int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `id_order`      int(11) NOT NULL,
+    `id_product`    int(11) NOT NULL,
+
+    FOREIGN KEY (`id_order`) REFERENCES Orders (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`id_product`) REFERENCES Products (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE = INNODB
+CHARACTER SET 'utf8';
