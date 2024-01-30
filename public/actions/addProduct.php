7c374166ba032057a3dfa199386bb2e142d6b85e
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-01-30 15:22:20 +0100

    add product page

---
+++ public/actions/addProduct.php
@@ -0,0 +1,48 @@
+<?php
+require_once __DIR__ . '/../src/init.php';
+// $pdo est dispo !
+
+if (empty($_POST['name'])) {
+    // error
+    $_SESSION['error_message'] = 'Name field is empty.';
+    header('Location: /addProduct.php'); // redirige utilisateur
+    die(); // stop execution du script
+}
+
+if (empty($_POST['type'])) {
+    // error
+    $_SESSION['error_message'] = 'Type field is empty.';
+    header('Location: /addProduct.php'); // redirige utilisateur
+    die(); // stop execution du script
+}
+
+if (empty($_POST['price'])) {
+    // error
+    $_SESSION['error_message'] = 'Price field is empty.';
+    header('Location: /addProduct.php'); // redirige utilisateur
+    die(); // stop execution du script
+}
+
+if (empty($_POST['quantity'])) {
+    // error
+    $_SESSION['error_message'] = 'Quantity field is empty.';
+    header('Location: /addProduct.php'); // redirige utilisateur
+    die(); // stop execution du script
+}
+
+// verifier qu'aucun n'objet n'a le meme nom et le meme type dans la DB
+$st1 = $pdo->prepare('SELECT * FROM products WHERE name = ? AND type = ?');
+$st1->execute([$_POST['name'], $_POST['type']]);
+$alreadyExists = $st1->fetch(PDO::FETCH_ASSOC);
+// $alreadyExists = [ 'username' => 'edouard', .... ];
+if ($alreadyExists != false) {
+    $_SESSION['error_message'] = 'A product of the same type has the same name.';
+    header('Location: /addProduct.php'); // redirige utilisateur
+    die(); // stop execution du script
+}
+
+// INSERT
+$st2 = $pdo->prepare('INSERT INTO products(name, type, price, quantity) VALUES(?, ?, ?, ?)');
+$st2->execute([$_POST['name'], $_POST['type'], $_POST['price'], $_POST['quantity']]);
+
+header('Location: /addProduct.php?success=1'); // $_GET['success']
