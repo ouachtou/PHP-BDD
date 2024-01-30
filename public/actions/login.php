b1c00f4c13ebc2cafe93959f740a7b7a49bcaac0
NTH3C <147485340+NTH3C@users.noreply.github.com>
2024-01-30 15:55:59 +0100

    login

--- public/actions/login.php
+++ public/actions/login.php
@@ -1,12 +1,23 @@
 <?php 
 
-session_start();
 session_unset();
 session_destroy();
 session_abort();
 $ok = 0;
-if (isset($_POST['rememberme']) && $_POST['rememberme'] === 'yes') {
-    $ok = session_set_cookie_params(1000000);
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $state= $pdo->prepare('SELECT * FROM users WHERE email = :email');
+    $state->execute();
+    $user = $state ->fetch();
+    if ($user && password_verify($password, $user['password'])){
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+    }
+    else{
+        echo('Mauvais identifiants');
+    }
 }
 
 // require ici
