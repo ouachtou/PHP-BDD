36f317f06a5a831cd5addebad36bf85a142d368c
ilonaroussel <ilonarousselpro@gmail.com>
2024-01-31 09:03:47 +0100

    public

--- public/index.php
+++ public/index.php
@@ -3,12 +3,14 @@ require_once __DIR__ . '/../src/init.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bonjour</title>
     <?php require_once __DIR__ . '/../src/partials/head_css.php'; ?>
 </head>
+
 <body>
     <?php require_once __DIR__ . '/../src/partials/menu.php'; ?>
     <?php require_once __DIR__ . '/../src/partials/show_error.php'; ?>
@@ -63,6 +65,6 @@ require_once __DIR__ . '/../src/init.php';
             <?php endforeach; ?>
         </tbody>
     </table>
-
 </body>
+
 </html>
