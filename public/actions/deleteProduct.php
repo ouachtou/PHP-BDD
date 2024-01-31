aff326fc0a06cfea226d96815397114aec13c951
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-01-31 09:18:10 +0100

    optimized deleteProduct.php

--- public/actions/deleteProduct.php
+++ public/actions/deleteProduct.php
@@ -1,17 +1,11 @@
 <?php
+require_once __DIR__ . '/init.php';
 
-require_once __DIR__ . '/db.php';
-
-function deleteProduct($idProduct) {
-
-    $pdo = initDB();
-
+function deleteProduct($pdo, $idProduct) {
     try {
         $delete = $pdo->prepare('DELETE FROM Products WHERE id = ?');
         $delete -> execute([$idProduct]);
     } catch (Exception $e) {
-        echo 'Exception reçue : ',  $e->getMessage(), "\n";
+        echo 'Exception : ',  $e->getMessage(), "\n";
     };
-
-
 }
